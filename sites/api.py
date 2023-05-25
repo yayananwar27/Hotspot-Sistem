3d469cca36f637f9263dcc697930fbb824826513
Yayan Anwar <yayananwar27@gmail.com>
2023-05-25 13:32:24 +0700

    Update api.py

--- sites/api.py
+++ sites/api.py
@@ -3,7 +3,7 @@ from flask_restful import Api
 from flask_cors import CORS
 
 
-from .site import SiteAPI, InfoSiteAPI, ListSiteprofile
+from .site import SiteAPI, InfoSiteAPI, ListSiteprofile, ListSiteplan
 
 site_api = Blueprint('site_api', __name__)
 CORS(site_api, supports_credentials=True, resources=r'*', origins="*", allow_headers=["Content-Type", "Authorization", "Accept"], methods=['GET','POST','PUT','DELETE'])
@@ -13,8 +13,10 @@ api = Api(site_api)
 api.add_resource(SiteAPI, '')
 api.add_resource(InfoSiteAPI, '/<id>')
 api.add_resource(ListSiteprofile, '/hotspot_profile/<id>')
+api.add_resource(ListSiteplan, '/<id>/plan')
 
 def init_docs(docs):
     docs.register(SiteAPI, blueprint='site_api')
     docs.register(InfoSiteAPI, blueprint='site_api')
     docs.register(ListSiteprofile, blueprint='site_api')
+    docs.register(ListSiteplan, blueprint='site_api')
