267626c445d23955d5d2fcc146b9c09e34a9d8c9
Yayan Anwar <34188580+yayananwar27@users.noreply.github.com>
2023-07-05 18:20:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -72,6 +72,10 @@ def shutdown_scheduler():
     scheduler.shutdown()
 
 if __name__ == "__main__":
+    
+    import logging
+    logging.basicConfig(filename='error.log',level=logging.DEBUG)
+    
     app.run('0.0.0.0', debug=True)
     app.teardown_appcontext(shutdown_scheduler)
     
